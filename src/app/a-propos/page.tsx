2837f486cba9c227e608f0bc095a228921f718d9
menbati <93155474+menbati@users.noreply.github.com>
2024-04-12 08:05:27 +0200

    Licence

--- src/app/a-propos/page.tsx
+++ src/app/a-propos/page.tsx
@@ -39,13 +39,7 @@ export default function APropos() {
         </li>
       </ul>
       <hr />
-      <p>
-        Site édité par :{" "}
-        <a href="https://www.linkedin.com/in/amaury-bauzac-08829717/">
-          Amaury Bauzac
-        </a>
-      </p>
-      <p>Site hébergé par : Github Inc.</p>
+       <p xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/"><a property="dct:title" rel="cc:attributionURL" href="https://github.com/abauzac/budgets-publics">Tableau de bord des collectivités</a> by <a rel="cc:attributionURL dct:creator" property="cc:attributionName" href="https://www.linkedin.com/in/amaury-bauzac-08829717/">Amaury Bauzac</a> is licensed under <a href="https://creativecommons.org/licenses/by/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY 4.0<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1"><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1"></a></p> 
     </>
   );
 }
